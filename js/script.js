1c3d29ef465bd9ea1008da7a268e1bb9ecc6b333
Olexander <alex.bor.1976@gmail.com>
2026-04-20 11:45:44 +0300

    the start

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// This tells jQuery to wait until the page is fully loaded
+$(document).ready(function() {
+    $('#clickMeBtn').click(function() {
+        alert("jQuery is working locally!");
+    });
+});

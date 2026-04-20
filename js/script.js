d75227581027642c81afc0d7d95ca05810fae0e3
Olexander <alex.bor.1976@gmail.com>
2026-04-20 19:05:04 +0300

    3 pages, common header in js, class

--- js/script.js
+++ js/script.js
@@ -1,41 +1,70 @@
-$(document).ready(function() {
-    // 1. Store the entire menu HTML in a variable using backticks (`)
-    const menuHtml = `
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav py-1 py-lg-0">
-                    <li class="nav-item ms-3 my-0 py-1 py-lg-0">
-                        <a href="index.html" class="btn btn-primary fs-4 custom-mobile-btn">Головна</a>
-                    </li>
-                    <li class="nav-item ms-3 my-0 py-1 py-lg-0">
-                        <a href="3d.html" class="btn btn-primary fs-4 custom-mobile-btn">Зразки 3D-моделей</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    `;
-
-    // 2. Inject the menu into the placeholder on the page
-    $('#header-placeholder').html(menuHtml);
-
-    // 3. Figure out which page we are currently on
-    let currentUrl = window.location.pathname.split('/').pop();
-    if (currentUrl === '') {
-        currentUrl = 'index.html'; // Default to index.html if no file is shown in the URL
+// 1. Define the class OUTSIDE of $(document).ready
+class MenuBuilder {
+    // The constructor runs when we create a new instance of this class
+    constructor(placeholderSelector) {
+        this.placeholderSelector = placeholderSelector;
     }
 
-    // 4. Find the matching link in our injected menu and make it look "clicked"
-    $('a[href="' + currentUrl + '"]').addClass('active').attr('aria-pressed', 'true');
+    // Method to store and return our HTML template
+    getMenuHtml() {
+        return `
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav pt-2 py-lg-0">
+                        <li class="nav-item ms-3 my-0 py-1 py-lg-0">
+                            <a href="index.html" class="btn btn-primary fs-4 custom-mobile-btn">Головна</a>
+                        </li>
+                        <li class="nav-item ms-3 my-0 py-1 py-lg-0">
+                            <a href="3d.html" class="btn btn-primary fs-4 custom-mobile-btn">Зразки 3D-моделей</a>
+                        </li>
+                        <li class="nav-item ms-3 my-0 py-1 py-lg-0">
+                            <a href="temp.html" class="btn btn-primary fs-4 custom-mobile-btn">Temp</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        `;
+    }
 
-    // ---------------------------------------------------------
-    // Your other jQuery code goes below here
-    // ---------------------------------------------------------
+    // Method to highlight the correct button based on the URL
+    setActiveState() {
+        let currentUrl = window.location.pathname.split('/').pop();
+        if (currentUrl === '') {
+            currentUrl = 'index.html'; // Default to index.html
+        }
+
+        // Find the link and add the active classes
+        $('a[href="' + currentUrl + '"]').addClass('active').attr('aria-pressed', 'true');
+    }
+
+    // Method to actually execute the building process
+    render() {
+        // Inject the HTML
+        $(this.placeholderSelector).html(this.getMenuHtml());
+        // Set the active state
+        this.setActiveState();
+    }
+}
+
+// ---------------------------------------------------------
+// 2. Main Execution (This runs when the page loads)
+// ---------------------------------------------------------
+$(document).ready(function() {
+    
+    // Create a new instance of our class and tell it where to inject the menu
+    const myMenu = new MenuBuilder('#header-placeholder');
+    
+    // Tell it to render!
+    myMenu.render();
+
+    // Your other jQuery code stays here, nice and clean
     $('#clickMeBtn').click(function() {
         alert("jQuery is working locally!");
     });
+
 });
